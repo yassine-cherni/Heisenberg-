281a8e24bdb21752b1899c28d085fdf8c132f8bf
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-02-24 18:59:21 +0100

    Update Test-data-voice.py

--- Test-data-voice.py
+++ Test-data-voice.py
@@ -1,6 +1,6 @@
 from assistant import Helper
 import time 
-from datetime import datet
+from datetime import datetime
 assistant=Helper()
 if __name__ == '__main__': 
     assistant.speak('Hi I am Yassine your assistante. i can search on wikipedia and write some note for you, how can i help you ', 140)
